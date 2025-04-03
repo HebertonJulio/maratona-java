749d827a4a74296f6bb2ecd1ce7b397e6fba682c
HebertonJulio <hebertonevil48@gmail.com>
2025-04-03 05:34:01 -0300

    Arquivos iniciais de Estudo.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[ ] args) {
+
+        System.out.println("Hello World, this is my first code in Java for my study, i go up this code for my github.");
+    }
+}
